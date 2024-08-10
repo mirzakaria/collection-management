16179ebc73d66b527f0378ed0411c7ff79ec17e3
mirzakaria <mir.zakaria02@gmail.com>
2024-08-10 23:12:40 +0600

    init commit

---
+++ src/main/java/com/zakaria/collection_management/CollectionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.zakaria.collection_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zakaria/collection_management/CollectionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakaria.collection_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
